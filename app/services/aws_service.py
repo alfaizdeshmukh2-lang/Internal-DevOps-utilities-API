beb926c38705f3f7f481b9e79782349a8c3fb0cf
Alfaiz Deshmukh | Devops Learner <alfaizdeshmukh2@gmail.com>
2026-01-17 17:12:20 +0530

    Uncomment get_bucket_info function in aws_service.py
    Uncommented the get_bucket_info function and its dependencies.

--- app/services/aws_service.py
+++ app/services/aws_service.py
@@ -1,25 +1,3 @@
-# import boto3
-# from datetime import datetime
-
-# def get_bucket_info():
-#           s3_client  = boto3.client("s3")
-          
-#           buckets = s3_client.list_buckets()["Buckets"]
-
-#           current_datetime = datetime.now()
-#           old_buckets =[]
-#           new_buckets = []
-#           for bucket in buckets:
-#                   bucket_name = buckets ["Name"]
-#                   new_buckets.append (bucket_name )
-
-#           return {
-#                   "new_buckets":new_buckets
-#           }
-
-
-########################
-
 import boto3
 from datetime import datetime, timezone , timedelta 
 
